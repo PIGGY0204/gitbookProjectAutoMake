049c0e7291a434917b7eb74a273df95a79b3ce06
piggy0204 <piggy0204@outlook.com>
2018-01-28 13:33:05 +0800

    improve log system

--- gitbookProjectAutoMake.sh
+++ gitbookProjectAutoMake.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+log() {
+    case $1 in
+    INFO)
+	echo "INFO $2"
+	;;
+    ERROR)
+        echo "ERROR $2"
+	exit 1
+	;;
+    esac
+}
+
 fileGrade=1
 yourFather=piggy
 
@@ -13,26 +25,23 @@ elif [ $# -eq 0 ]; then
   project="./"
   config="config"
 else
-  echo "usage: ./gitbookProjectAutoMake [project_path] [config_file_path]"
+  log ERROR "usage:./gitbookProjectAutoMake [project_path] [config_file_path]"
 fi
 
 if [ ! -d ${project} ]; then
   mkdir $project
-  echo "the project directory does not exist! And I help you make this directory!"
+  log INFO "mkdir:make the project"
 fi
 
 if [ ! -f ${config} ]; then 
-  echo "the config file does not exist!"
-  exit 1
+  log ERROR "config:can not find config"
 fi
 
 summaryFile="${project}/SUMMARY.md"
 readmeFile="README.md"
 
 touch ${summaryFile}
-echo "make the SUMMARY.md"
 echo -e "# SUMMARY\n" > ${summaryFile}
-echo ""
 
 while read line
 do
@@ -40,15 +49,12 @@ do
   title=`echo ${line} | awk '{print $2}'`
   fileName=`echo ${line} | awk '{print $3}'`
 
-  echo -e "grade:${grade}\ttitle:${title}\tfileName:${fileName}"
-
   if [ "$grade" == "#" ]
   then
     fileGrade=1
   elif [ "$grade" == "##" ]
   then
     fileGrade=2
-    echo "parent: ${yourFather}"
   fi
 
   if [ $fileGrade -eq 1 ]
@@ -58,51 +64,47 @@ do
       if [ ! -f ${project}${readmeFile} ]; then
         touch ${project}${readmeFile}
         echo "# $title" > ${project}${readmeFile}
-        echo "make a new file ------- ${project}${readmeFile}"
+        log INFO "touch:${project}${readmeFile}"
       else
         sed -i "1c # ${title}" ${project}${readmeFile}
-        echo -e "${project}${readmeFile} is existed!\nupdate the title of ${project}${readmeFile}"
+        log INFO "title:\"${title}\" > ${project}${readmeFile}"
       fi
-
       echo "* [${title}](${readmeFile})" >> $summaryFile
-      echo "add \"* [${title}](${readmeFile})\" into SUMMARY.md"
     else
       yourFather=$fileName
       if [ ! -d ${project}${yourFather} ]; then
         mkdir ${project}${yourFather}
-        echo "make a new directory -- ${project}${yourFather}"
-      else
-        echo "${project}${yourFather} is existed!"
+        log INFO "mkdir:${project}${yourFather}"
       fi
 
       if [ ! -f ${project}${yourFather}/${readmeFile} ]; then
         touch ${project}${yourFather}/${readmeFile}
         echo "# $title" > ${project}${yourFather}/${readmeFile}
-        echo "make a new file ------- ${project}${yourFather}/${readmeFile}"
+        log INFO "touch:${project}${yourFather}/${readmeFile}"
       else
         sed -i "1c # ${title}" ${project}${yourFather}/${readmeFile}
-        echo -e "${project}${yourFather}/${readmeFile} is existed!\nupdate the title of ${project}${yourFather}/${readmeFile}"
+        log INFO "title:\"${title}\" > ${project}${yourFather}/${readmeFile}"
       fi
       echo "* [${title}](${fileName}/README.md)" >> $summaryFile
-      echo "add \"* [${title}](${fileName}/${readmeFile})\" into SUMMARY.md"
     fi
   elif [ $fileGrade -eq 2 ]
   then
     if [ ! -f "${project}${yourFather}/${fileName}.md" ]; then
       touch ${project}${yourFather}/${fileName}.md
       echo "# $title" > ${project}${yourFather}/${fileName}.md
-      echo "make a new file ------- ${project}${yourFather}/${fileName}.md"
+      log INFO "touch:${project}${yourFather}/${fileName}.md"
     else
       sed -i "1c # ${title}" ${project}${yourFather}/${fileName}.md
-      echo -e "${project}${yourFather}/${fileName}.md is existed!\nupdate the title of ${project}${yourFather}/${fileName}.md"
+      log INFO "title:\"${title}\" > ${project}${yourFather}/${fileName}.md"
     fi
     echo "  * [${title}](${yourFather}/${fileName}.md)" >> $summaryFile
-    echo "add \"  * [${title}](${yourFather}/${fileName}.md)\" into SUMMARY.md"
   fi
-
-    echo ""
 done < $config
 
+echo ""
+cat $summaryFile
+
+echo ""
 tree $project
 
 echo -e "\nfinish"
